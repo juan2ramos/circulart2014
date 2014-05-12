8b30104c481d02888956b6f814ad185ad8158827
juan2ramos <juan2ramos@gmail.com>
2014-05-12 10:14:13 -0500

    inicio

---
+++ layout/default.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="es">
+<head>
+
+    <title><?php echo $title ?></title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="author" content="juan2ramos"/>
+    <meta name="contact" content="juan2ramos@gmail.com"/>
+    <meta name="description"
+          content="Circulart2014 Mercado Cultural de Medellín dedicado a la industria musical Latinoamericana http://2013.circulart.org/ Encuentro anual, dedicado a la industria musical independiente de Latinoamérica, show cases, conferencias, talleres, networking, sesiones de negocios y una oportunidad única de descubrir la diversidad musical Iberoamericana.."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+    <!-- Meta Facebook -->
+    <meta property="og:url" content="http://circulart.com.co/">
+    <meta property="og:title" content="circulart"/>
+    <meta property="og:description"
+          content="Circulart2013 Mercado Cultural de Medellín dedicado a la industria musical Latinoamericana http://2013.circulart.org/ Encuentro anual, dedicado a la industria musical independiente de Latinoamérica, show cases, conferencias, talleres, networking, sesiones de negocios y una oportunidad única de descubrir la diversidad musical Iberoamericana.."/>
+    <meta property="og:image" content="images/logo.png"/>
+
+
+    <link rel="shortcut icon" href="favicon.png">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic'
+          rel='stylesheet' type='text/css'>
+
+    <!-- Estilos -->
+    <script src="js/prefixfree.min.js"></script>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+<body>
+<header>
+    <div id="slide">
+        <figure id="header-buttom">
+            <img src="images/header/<?php echo($url)?>.jpg">
+        </figure>
+    </div>
+    <div class="contend-header">
+        <p>Mercado cultural</p><p>  circulart 2014</p>
+
+    </div>
+
+
+</header>
+<nav>
+    <ul>
+        <li><a href="que-es" class="active">convocatoria</a></li>
+        <li><a href="portafolio">curaduría</a></li>
+        <li><a href="mercado">artistas</a></li>
+        <li><a href="radio">profesionales</a></li>
+        <li><a href="noticias">agendamiento</a></li>
+        <li><a href="contacto">showcases</a></li>
+        <li><a href="contacto">georeferenciación</a></li>
+        <li><a href="contacto">catalogo</a></li>
+    </ul>
+    <div id="network">
+        <ul>
+            <li id="twitter"><a href="#"><span class="icon-twitter"></span></a></li>
+            <li id="facebook"><a href="#"><span class="icon-facebook"></span></a></li>
+            <li id="pinterest"><a href="#"><span class="icon-pinterest"></span></a></li>
+            <li id="youtube"><a href="#"><span class="icon-uniE603"></span></a></li>
+        </ul>
+    </div>
+
+
+</nav>
+
+
+<?php include $fileName; ?>
+
+<footer>
+    <ul id="nav-footer">
+
+        <li><a href="#"><img src="images/redlat.jpg"></a></li>
+        <li><a href="#"><img src="images/circulart.jpg"></a></li>
+    </ul>
+</footer>
+<!-- JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ views/convocatoria.php
@@ -0,0 +1,34 @@
+<section id="<?php echo($url) ?>">
+    <h2 class="<?php echo($url) ?>">circulart</h2>
+    <article>
+
+        <h3>Circulart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx</h3>
+        <p>Circulart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx xxxxxxxxxxxxxxx xxxxx xx x x xxxxxx x xx xCirculart xxxxxxx xxxxxxxxxxxxx xxx </p>
+
+    </article>
+    <h2 class="register">registrarse</h2>
+    <canvas id="myCanvas"></canvas>
+</section>
+
+
+<script>
+
+
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext('2d');
+        doResize();
+        window.onresize=function() { doResize(); };
+         function doResize()
+        {
+
+            context.canvas.width = window.innerWidth-20;
+            context.beginPath();
+            context.moveTo(0, 10);
+            context.lineTo(500, 10);
+            context.lineTo(1020, 80);
+            context.lineTo(window.innerWidth-20, 20);
+
+            context.stroke();
+        }
+
+</script>
